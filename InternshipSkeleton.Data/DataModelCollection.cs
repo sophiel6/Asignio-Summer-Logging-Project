2d503e0027a761089993da5ad4fda3b8a31c2562
Eddie <eddie.brady@asignio.com>
2019-07-02 11:09:12 -0700

    Template class added for preliminary Paging

---
+++ InternshipSkeleton.Data/DataModelCollection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AsignioInternship.Data
+{
+    public class DataModelCollection<T>
+    {
+        public IEnumerable<T> Items { get; set; }
+    }
+
+    public class PagedDataModelCollection<T> : DataModelCollection<T>
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public long TotalItems { get; set; }
+        public long TotalPages { get; set; }
+    }
+}

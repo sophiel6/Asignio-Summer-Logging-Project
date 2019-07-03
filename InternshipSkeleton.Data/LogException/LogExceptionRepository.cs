0df55c0b182aaed90a3bdd201a52a7d3ada4b56b
Eddie <eddie.brady@asignio.com>
2019-07-03 08:24:36 -0700

    Paging template expanded on

--- InternshipSkeleton.Data/LogException/LogExceptionRepository.cs
+++ InternshipSkeleton.Data/LogException/LogExceptionRepository.cs
@@ -75,5 +75,54 @@ namespace AsignioInternship.Data.LogException
 
             return null;
         }
+
+        public PagedDataModelCollection<LogExceptionDataModel> PageLogException(string nameSearchPattern, int pageSize, int pageNumber, string sortColumn, string sortDirection)
+        {
+            using (AsignioDatabase db = new AsignioDatabase(ConnectionStringName))
+            {
+                try
+                {
+                    PetaPoco.Sql sql = new PetaPoco.Sql();
+
+                    if (!string.IsNullOrWhiteSpace(nameSearchPattern))
+                    {
+                        nameSearchPattern = string.Format("%{0}%", nameSearchPattern);
+
+                        // You can add a PageUsersByUserIDSearchSQL or something similar to LogExceptionPoco.cs, like you did with BaseSQL, for example:
+                        // sql.Append(LogExceptionDataModel.PageLogExceptionByXSearchSQL, nameSearchPattern);
+                    }
+
+                    sql.Append(LogExceptionPoco.BaseSQL);
+                    sql.Append(string.Format("ORDER BY {0} {1}", sortColumn, sortDirection)); // sortColumn being the name of the column from the table, and sortDirection being ASC or DESC, for example
+
+                    PetaPoco.Page<LogExceptionDataModel> page = db.Page<LogExceptionDataModel>(pageNumber, pageSize, sql);
+
+                    if (page == null)
+                    {
+                        return null;
+                    }
+
+                    return new PagedDataModelCollection<LogExceptionDataModel>()
+                    {
+                        Items = page.Items,
+                        PageNumber = pageNumber,
+                        PageSize = pageSize,
+                        TotalItems = page.TotalItems,
+                        TotalPages = page.TotalPages
+                    };
+                }
+                catch (Exception ex)
+                {
+                    string errorMessage = ex.Message;
+                }
+                finally
+                {
+                }
+            }
+
+            return null;
+        }
+
+
     }
 }

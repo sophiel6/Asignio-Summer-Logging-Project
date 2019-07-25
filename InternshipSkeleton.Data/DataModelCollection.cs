1c050de1f377553c03141b3fedde4d5fad9b7b9c
Sophie Lanier <sophie.lanier@asignio.com>
2019-07-25 12:11:54 -0700

    fixed dropdownlist css issue

--- InternshipSkeleton.Data/DataModelCollection.cs
+++ InternshipSkeleton.Data/DataModelCollection.cs
@@ -16,9 +16,7 @@ namespace AsignioInternship.Data
         public long TotalItems { get; set; }
         public long TotalPages { get; set; }
 
-        //adding for sortby dropdown list
         public string SortBy { get; set; }
-        //adding for search dropdown list
         public string SearchBy { get; set; }
         public string SearchInput { get; set; }
 

3c3eb7a83ba3596cbbf734ccaee400346ca5b1b7
Sophie Lanier <sophie.lanier@asignio.com>
2019-08-13 15:32:41 -0700

    LogWebRequest mark as important seems to be working; added search by user who marked as important to LogWebRequest

--- InternshipSkeleton/Controllers/LogWebRequestController.cs
+++ InternshipSkeleton/Controllers/LogWebRequestController.cs
@@ -21,12 +21,7 @@ namespace AsignioInternship.Controllers
             int pageSize = 20;
             string sortColumn = sortBy ?? "TimeStamp";
             string sortDirection = sortDir ?? "ASC";
-            var searchD = new Dictionary<string, string>()
-            {
-                {"TimeStamp", ""  },
-                {"RawURL", "" },
-                {"Parameters", "" }
-             };
+            var searchD = new Dictionary<string, string>() { { "Default", "" } };
 
             Dictionary<string, string> searchDict;
             if (searchDictionary.Keys.ElementAt(0) == "controller" || searchDictionary == null)
@@ -44,12 +39,7 @@ namespace AsignioInternship.Controllers
             int pageSize = 20;
             string sortColumn = sortBy ?? "TimeStamp";
             string sortDirection = sortDir ?? "ASC";
-            var searchD = new Dictionary<string, string>()
-            {
-                {"TimeStamp", ""  },
-                {"RawURL", "" },
-                {"Parameters", "" }
-             };
+            var searchD = new Dictionary<string, string>() { { "Default", "" } };
 
             Dictionary<string, string> searchDict;
             if (searchDictionary.Keys.ElementAt(0) == "controller" || searchDictionary == null)
